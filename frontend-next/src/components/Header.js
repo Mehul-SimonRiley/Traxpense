94d365a29a530da0467145d886b0bacd5725afd8
Mehul-SimonRiley <extrararegamer@gmail.com>
2026-06-11 15:55:45 +0530

    Increase navbar height to 80px and logo size to 44px

--- frontend-next/src/components/Header.js
+++ frontend-next/src/components/Header.js
@@ -34,7 +34,7 @@ export default function Header({ toggleSidebar, isOpen }) {
                         src={theme === 'dark' ? '/logo_white.png' : '/logo_black.png'} 
                         alt="Traxpense Logo" 
                         className={styles.logoIcon}
-                        style={{ width: '32px', height: '32px', objectFit: 'contain' }}
+                        style={{ width: '44px', height: '44px', objectFit: 'contain' }}
                     />
                     <span>Traxpense</span>
                 </div>
